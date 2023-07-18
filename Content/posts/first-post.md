af895447dccdc2d605a27cd7ee8a8b2922c5333e
Zac Johnson <zac.johnson@locushealth.com>
2023-07-18 09:49:22 -0400

    generate site with publish

---
+++ Content/posts/first-post.md
@@ -0,0 +1,8 @@
+---
+date: 2023-07-18 09:18
+description: A description of my first post.
+tags: first, article
+---
+# My first post
+
+My first post's text.
---
+++ Sources/ZcjhnsnGithubIo/main.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct ZcjhnsnGithubIo: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case apps
+        case posts
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://zcjhnsn.com")!
+    var name = "Zac Johnson"
+    var description = "iOS Developer"
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try ZcjhnsnGithubIo()
+    .publish(using: [
+        .addMarkdownFiles(),
+        .copyResources(),
+        .generateHTML(withTheme: .foundation),
+        .generateRSSFeed(including: [.posts]),
+        .generateSiteMap(),
+        .deploy(
+            using: .gitHub(
+                "zcjhnsn/zcjhnsn.github.io",
+                branch: "main",
+                useSSH: true
+            )
+        )
+    ])
